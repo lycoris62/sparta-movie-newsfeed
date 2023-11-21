dbd629cdb4e2d8c6938fc690e1511179835fc11c
barami62 <wodbs7758@naver.com>
2023-11-21 16:54:20 +0900

    init commit

---
+++ src/test/java/sparta/ifour/movietalk/MovietalkApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.ifour.movietalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovietalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
