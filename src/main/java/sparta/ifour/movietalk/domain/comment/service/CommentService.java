1feec56bcc8001bc84b4bb0125c614a5f1238a55
gk <iba3374@naver.com>
2023-11-21 21:35:09 +0900

    feat : 댓글 서비스 댓글 생성 추가

--- src/main/java/sparta/ifour/movietalk/domain/comment/service/CommentService.java
+++ src/main/java/sparta/ifour/movietalk/domain/comment/service/CommentService.java
@@ -2,12 +2,27 @@ package sparta.ifour.movietalk.domain.comment.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sparta.ifour.movietalk.domain.comment.dto.request.CommentCreateRequestDto;
+import sparta.ifour.movietalk.domain.comment.dto.response.CommentCreateResponseDto;
+import sparta.ifour.movietalk.domain.comment.entity.Comment;
 import sparta.ifour.movietalk.domain.comment.repository.CommentRepository;
+import sparta.ifour.movietalk.domain.reviews.entity.Review;
+import sparta.ifour.movietalk.domain.reviews.repository.ReviewRepository;
+import sparta.ifour.movietalk.domain.user.entity.User;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final ReviewRepository reviewRepository;
 
+    public CommentCreateResponseDto createComment(CommentCreateRequestDto requestDto, User user, Long reviewId) {
+        Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new IllegalArgumentException("존재 하지 않는 리뷰 입니다."));
+        Comment comment = Comment.create(requestDto.getContent(), user, review);
+        commentRepository.save(comment);
+        return new CommentCreateResponseDto(comment);
+    }
 }
