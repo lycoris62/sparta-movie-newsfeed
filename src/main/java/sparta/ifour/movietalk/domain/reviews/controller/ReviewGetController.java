6d753d2d494b8217abc5d9a88c9ea8a080a4e5ea
juneyoung Lee <junyeong237@gmail.com>
2023-11-21 19:08:32 +0900

    ReviewResponseDto 추가

--- src/main/java/sparta/ifour/movietalk/domain/reviews/controller/ReviewGetController.java
+++ src/main/java/sparta/ifour/movietalk/domain/reviews/controller/ReviewGetController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sparta.ifour.movietalk.domain.reviews.dto.ReviewResponseDto;
 import sparta.ifour.movietalk.domain.reviews.service.ReviewService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -15,25 +18,25 @@ public class ReviewGetController {
     private final ReviewService reviewService;
 
     @GetMapping("/reviews") //리뷰 전체 목록 조회
-    public void getAllReviews(){
+    public List<ReviewResponseDto> getAllReviews(){
 
-        return;
+        return List.of();
     }
 
     @GetMapping("/reviews/({reviewId}") // 특정 리뷰 상세조회
-    public void getReview(@PathVariable Long reviewId){
+    public ReviewResponseDto getReview(@PathVariable Long reviewId){
 
-        return;
+        return null;
     }
 
     @GetMapping("/reviews/query/{queryName}") //리뷰 검색 목록 조회
-    public void getReviewsBySearch(@PathVariable String queryName){
-        return;
+    public List<ReviewResponseDto> getReviewsBySearch(@PathVariable String queryName){
+        return List.of();
     }
 
     @GetMapping("reviews/hashtag/{hashtagName}") // 특정 해시태그가 포함된 리뷰 조회
-    public void getReviewsByHashTag(@PathVariable String hashtagName){
-        return;
+    public List<ReviewResponseDto> getReviewsByHashTag(@PathVariable String hashtagName){
+        return List.of();
 
     }
 
