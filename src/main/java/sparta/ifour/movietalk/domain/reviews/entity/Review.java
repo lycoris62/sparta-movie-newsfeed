fa5e3109532b42a7f2d9f0d3777d4eb6a5c9916b
juneyoung Lee <junyeong237@gmail.com>
2023-11-21 20:13:30 +0900

    refactor: Review 리팩토링2

--- src/main/java/sparta/ifour/movietalk/domain/reviews/entity/Review.java
+++ src/main/java/sparta/ifour/movietalk/domain/reviews/entity/Review.java
@@ -1,20 +1,15 @@
 package sparta.ifour.movietalk.domain.reviews.entity;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
-
-import java.util.ArrayList;
 
 
 @Getter
 @Entity
 @Table(name = "review")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 
-public class ReviewEntity{
+public class Review{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
