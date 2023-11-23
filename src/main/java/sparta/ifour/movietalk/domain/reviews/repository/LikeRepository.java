8acbd9fb71e5c43faae89c6f3173d54b85fac85b
minahYu <mina010311@naver.com>
2023-11-23 18:06:13 +0900

    chore : 불필요한 import 문 최적화

--- src/main/java/sparta/ifour/movietalk/domain/reviews/repository/LikeRepository.java
+++ src/main/java/sparta/ifour/movietalk/domain/reviews/repository/LikeRepository.java
@@ -2,7 +2,6 @@ package sparta.ifour.movietalk.domain.reviews.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import sparta.ifour.movietalk.domain.reviews.entity.Like;
-import sparta.ifour.movietalk.domain.user.entity.User;
 
 import java.util.Optional;
 
