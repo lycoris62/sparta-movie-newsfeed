264522349b7488e5297ec2909d1d0de2676c60e9
gk <iba3374@naver.com>
2023-11-23 20:17:54 +0900

    refactor : 댓글 서비스 메소드 내용 줄바꿈

--- src/main/java/sparta/ifour/movietalk/domain/comment/service/CommentService.java
+++ src/main/java/sparta/ifour/movietalk/domain/comment/service/CommentService.java
@@ -23,7 +23,8 @@ public class CommentService {
     private final ReviewRepository reviewRepository;
 
     public CommentCreateResponseDto createComment(CommentCreateRequestDto requestDto, User user) {
-        Review review = reviewRepository.findById(requestDto.getReviewId()).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 리뷰입니다."));
+        Review review = reviewRepository.findById(requestDto.getReviewId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 리뷰입니다."));
         Comment comment = Comment.create(requestDto.getContent(), user, review);
         commentRepository.save(comment);
         return new CommentCreateResponseDto(comment);
@@ -40,7 +41,8 @@ public class CommentService {
     }
 
     private Comment getUserComment(Long commentId, User user) {
-        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 댓글입니다."));
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 댓글입니다."));
 
         if (!comment.getUser().getId().equals(user.getId())) {
             throw new RejectedExecutionException("본인의 댓글만 수정이 가능합니다.");
