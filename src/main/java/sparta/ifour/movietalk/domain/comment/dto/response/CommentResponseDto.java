32aa7ee5c5e2588004b44561b2bb5a44833fd292
juneyoung Lee <junyeong237@gmail.com>
2023-11-23 21:53:20 +0900

    Refactor : CommentResponseDto @Getter 추가, 오타수정

--- src/main/java/sparta/ifour/movietalk/domain/comment/dto/response/CommentResponseDto.java
+++ src/main/java/sparta/ifour/movietalk/domain/comment/dto/response/CommentResponseDto.java
@@ -1,20 +1,22 @@
 package sparta.ifour.movietalk.domain.comment.dto.response;
 
+import lombok.Getter;
 import sparta.ifour.movietalk.domain.comment.entity.Comment;
 
 import java.time.LocalDateTime;
 
+@Getter
 public class CommentResponseDto {
 
 
     private Long id;
     private String content;
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
     public CommentResponseDto(Comment comment) {
         this.id = comment.getId();
         this.content = comment.getContent();
-        this.createAt = comment.getCreatedAt();
+        this.createdAt = comment.getCreatedAt();
     }
 
 }
