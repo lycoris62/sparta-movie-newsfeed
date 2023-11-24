90f3253b549a899a078a62d6f38b95a25a7ce371
gk <iba3374@naver.com>
2023-11-24 18:23:43 +0900

    feat : 데이터 검증 예외 처리

--- src/main/java/sparta/ifour/movietalk/global/config/security/error/GlobalExceptionHandler.java
+++ src/main/java/sparta/ifour/movietalk/global/config/security/error/GlobalExceptionHandler.java
@@ -2,8 +2,10 @@ package sparta.ifour.movietalk.global.config.security.error;
 
 import static sparta.ifour.movietalk.global.config.security.error.ErrorCode.NOT_FOUND;
 
+import java.util.ArrayList;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sparta.ifour.movietalk.global.config.security.exception.NotFoundException;
@@ -12,7 +14,9 @@ import sparta.ifour.movietalk.global.config.security.exception.NotFoundException
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+    protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(
+            IllegalArgumentException e) {
+
         ErrorResponse response = new ErrorResponse(e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
@@ -21,6 +25,16 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
         ErrorResponse response = new ErrorResponse(NOT_FOUND.getMessage());
         return new ResponseEntity<>(response, NOT_FOUND.getHttpStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+
+        ArrayList<String> errors = new ArrayList<>();
+        e.getAllErrors().forEach(error -> errors.add(error.getDefaultMessage()));
+        ErrorResponse response = new ErrorResponse(errors);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
 }
