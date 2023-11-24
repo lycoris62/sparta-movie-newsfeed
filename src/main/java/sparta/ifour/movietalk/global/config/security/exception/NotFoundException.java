22d8063d1dcb932a615e245863963b8660f3a675
gk <iba3374@naver.com>
2023-11-24 17:43:35 +0900

    fix : NotFoundException 생성자 수정

--- src/main/java/sparta/ifour/movietalk/global/config/security/exception/NotFoundException.java
+++ src/main/java/sparta/ifour/movietalk/global/config/security/exception/NotFoundException.java
@@ -1,8 +1,4 @@
 package sparta.ifour.movietalk.global.config.security.exception;
 
 public class NotFoundException extends RuntimeException{
-
-    public NotFoundException(String message) {
-        super(message);
-    }
 }
