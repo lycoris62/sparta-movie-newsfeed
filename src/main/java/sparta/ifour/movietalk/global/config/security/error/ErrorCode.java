b8cd7a7e7369344b4dcf9689327780c14f6ecab7
gk <iba3374@naver.com>
2023-11-24 17:39:32 +0900

    feat : 에러코드에 NotFound 추가

--- src/main/java/sparta/ifour/movietalk/global/config/security/error/ErrorCode.java
+++ src/main/java/sparta/ifour/movietalk/global/config/security/error/ErrorCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    ;
+    NOT_FOUND(HttpStatus.NOT_FOUND, "전달받은 id에 해당하는 리소스가 없음");
 
     private final HttpStatus httpStatus;
     private final String message;
