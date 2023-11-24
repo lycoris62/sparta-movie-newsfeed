3da1476b6f4e2503c880bf0ee83a4d19b0b9d930
gk <iba3374@naver.com>
2023-11-24 17:39:21 +0900

    feat : NotFoundException 추가

---
+++ src/main/java/sparta/ifour/movietalk/global/config/security/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package sparta.ifour.movietalk.global.config.security.exception;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
