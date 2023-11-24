1b08712a47ac1e6d9ab44bf9110ad55778036018
juneyoung Lee <junyeong237@gmail.com>
2023-11-24 20:58:45 +0900

    Feat : 중복하는 tag name 확인 메서드

--- src/main/java/sparta/ifour/movietalk/domain/review/repository/HashtagRepository.java
+++ src/main/java/sparta/ifour/movietalk/domain/review/repository/HashtagRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface HashtagRepository extends JpaRepository<Hashtag,Long> {
     Optional<Hashtag> findByName(String name);
+    boolean existsByName(String name);
 }
