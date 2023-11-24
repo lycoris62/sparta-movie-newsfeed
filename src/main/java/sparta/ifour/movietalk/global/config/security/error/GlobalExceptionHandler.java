0db929094cf9037fc4dd6357be07d8c281eda204
gk <iba3374@naver.com>
2023-11-24 17:46:08 +0900

    feat : NotFound 예외 처리

--- src/main/java/sparta/ifour/movietalk/global/config/security/error/GlobalExceptionHandler.java
+++ src/main/java/sparta/ifour/movietalk/global/config/security/error/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package sparta.ifour.movietalk.global.config.security.error;
 
+import static sparta.ifour.movietalk.global.config.security.error.ErrorCode.NOT_FOUND;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import sparta.ifour.movietalk.global.config.security.exception.NotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,6 +15,12 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
         ErrorResponse response = new ErrorResponse(e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
+        ErrorResponse response = new ErrorResponse(NOT_FOUND.getMessage());
+        return new ResponseEntity<>(response, NOT_FOUND.getHttpStatus());
     }
 
 }
