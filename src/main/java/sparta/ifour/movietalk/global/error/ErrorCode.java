8b6d1471a213b5cf164de35445445be9d20f013e
gk <iba3374@naver.com>
2023-11-24 19:13:13 +0900

    feat : 에러코드에 InvalidAccess 추가

--- src/main/java/sparta/ifour/movietalk/global/error/ErrorCode.java
+++ src/main/java/sparta/ifour/movietalk/global/error/ErrorCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    NOT_FOUND(HttpStatus.NOT_FOUND, "전달받은 값에 해당하는 리소스가 없음");
+    NOT_FOUND(HttpStatus.NOT_FOUND, "전달받은 값에 해당하는 리소스가 없음"),
+    INVALID_ACCESS(HttpStatus.BAD_REQUEST, "본인의 리뷰, 댓글만 수정 및 삭제가 가능");
 
     private final HttpStatus httpStatus;
     private final String message;
