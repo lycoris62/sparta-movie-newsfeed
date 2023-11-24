e10a60042bb0c74c8ed610c3e00af929f7b94ebb
juneyoung Lee <junyeong237@gmail.com>
2023-11-24 14:07:20 +0900

    Feat : ReviewRepository 쿼리문 추가

--- src/main/java/sparta/ifour/movietalk/domain/reviews/repository/ReviewRepository.java
+++ src/main/java/sparta/ifour/movietalk/domain/reviews/repository/ReviewRepository.java
@@ -3,6 +3,13 @@ package sparta.ifour.movietalk.domain.reviews.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sparta.ifour.movietalk.domain.reviews.entity.Review;
 
+import java.time.LocalDate;
+import java.util.List;
+
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findAllByOrderByCreatedAtDesc();
+
+    List<Review> findAllByCreatedAtAfterOrderByCreatedAtDesc(LocalDate date);
 }
