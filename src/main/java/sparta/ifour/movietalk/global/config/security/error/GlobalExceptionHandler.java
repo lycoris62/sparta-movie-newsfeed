6af161709b148af79a1f791172e337e167246080
gk <iba3374@naver.com>
2023-11-24 17:35:37 +0900

    feat : IllegalArgumentException 예외 처리

--- src/main/java/sparta/ifour/movietalk/global/config/security/error/GlobalExceptionHandler.java
+++ src/main/java/sparta/ifour/movietalk/global/config/security/error/GlobalExceptionHandler.java
@@ -1,9 +1,17 @@
 package sparta.ifour.movietalk.global.config.security.error;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        ErrorResponse response = new ErrorResponse(e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 
 }
