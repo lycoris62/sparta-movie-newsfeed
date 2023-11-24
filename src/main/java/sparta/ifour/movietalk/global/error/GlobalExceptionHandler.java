fb3d2a1a9520df362ff28512b20abbc995aa736d
gk <iba3374@naver.com>
2023-11-24 19:13:43 +0900

    feat : InvalidAccessException 처리

--- src/main/java/sparta/ifour/movietalk/global/error/GlobalExceptionHandler.java
+++ src/main/java/sparta/ifour/movietalk/global/error/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import sparta.ifour.movietalk.global.exception.InvalidAccessException;
 import sparta.ifour.movietalk.global.exception.NotFoundException;
 
 @RestControllerAdvice
@@ -33,6 +34,12 @@ public class GlobalExceptionHandler {
         e.getAllErrors().forEach(error -> errors.add(error.getDefaultMessage()));
         ErrorResponse response = new ErrorResponse(errors);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidAccessException.class)
+    protected ResponseEntity<ErrorResponse> handleInvalidAccessException(InvalidAccessException e) {
+        ErrorResponse response = new ErrorResponse(ErrorCode.INVALID_ACCESS.getMessage());
+        return new ResponseEntity<>(response, ErrorCode.INVALID_ACCESS.getHttpStatus());
     }
 
 }
