cc79e7c17d4c2d4fdc10a44eb8c4a6da51395efd
gk <iba3374@naver.com>
2023-11-24 17:34:25 +0900

    feat : 예외 핸들러 추가

---
+++ src/main/java/sparta/ifour/movietalk/global/config/security/error/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package sparta.ifour.movietalk.global.config.security.error;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
